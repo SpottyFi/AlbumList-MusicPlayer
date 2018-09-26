b78002ad6fac727142f46372809ff12d2d1c0755
Shane Tajima <shanetajima@gmail.com>
2018-09-26 16:31:14 -0700

    Add CRUD endpoints

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://spottyfi:spottyfi1!@ds115753.mlab.com:15753/albumlist-musicplayer');
+mongoose.connect('mongodb://spottyfi:spottyfi1!@ds115753.mlab.com:15753/albumlist-musicplayer', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
@@ -27,11 +27,31 @@ var Artist = mongoose.model('Artist', ArtistSchema);
 
 var getArtist = (id, cb) => {
   Artist.find({ 'artistID': id }, (err, data) => {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
     cb(data);
-  })
-}
+  });
+};
 
-module.exports.Artist = Artist;
-module.exports.db = db;
-module.exports.getArtist = getArtist;
+const postArtist = (data, callback) => {
+  const artist = new Artist(data);
+  artist.save(callback);
+};
+
+const updateArtist = (id, data, callback) => {
+  Artist.updateOne({ artistID: id }, { $set: data }, callback);
+};
+
+const deleteArtist = (id, callback) => {
+  Artist.deleteOne({ artistID: id }, callback);
+};
+
+module.exports = {
+  Artist,
+  db,
+  getArtist,
+  postArtist,
+  updateArtist,
+  deleteArtist,
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { getArtist } = require('../database/index.js');
+const { getArtist, postArtist, updateArtist, deleteArtist } = require('../database/index.js');
 const cors = require('cors');
 const server = express();
 
@@ -13,6 +13,39 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.get('/artists/albums/:artistID', (req, res) => {
   getArtist(req.params.artistID, data => {
     res.send(data);
+  });
+});
+
+server.post('/artists/albums', (req, res) => {
+  postArtist(req.body, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(500).end(error);
+    } else {
+      res.end();
+    }
+  });
+});
+
+server.put('/artists/albums/:id', (req, res) => {
+  updateArtist(req.params.id, req.body, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(500).end(error);
+    } else {
+      res.end();
+    }
+  });
+});
+
+server.delete('/artists/albums/:id', (req, res) => {
+  deleteArtist(req.params.id, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(500).end(error);
+    } else {
+      res.end();
+    }
   });
 });
 
