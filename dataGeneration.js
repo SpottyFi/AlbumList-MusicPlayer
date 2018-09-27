3d5674fdb4a7c5c8dfa4c5cd48ae311dfac723a1
Shane Tajima <shanetajima@gmail.com>
2018-09-27 14:34:56 -0700

    Create basic gen script

--- dataGeneration.js
+++ dataGeneration.js
@@ -2,46 +2,46 @@ const faker = require('faker');
 const fs = require('fs');
 const coolImages = require('cool-images');
 
-var generateData = () => {
-  var allArtists = [];
+const stream = fs.createWriteStream('../seed_data.json');
 
-  for (let i = 1; i < 101; i++) {
-    let artist = {
-      artistID: i,
-      artistName: faker.name.findName(),
-      albums: []
+console.log('Starting data generation');
+stream.write('[');
+const MAX = 25000;
+let counter = 0;
+for (let i = 1; i <= MAX; i += 1) {
+  console.log(MAX - i);
+  const artist = {
+    artistID: i,
+    artistName: faker.name.findName(),
+    albums: [],
+  };
+  const albumNumber = faker.random.number({ min: 3, max: 6 });
+  for (let j = 0; j < albumNumber; j += 1) {
+    counter += 1;
+    const album = {
+      albumID: i * 10 + j,
+      albumName: faker.random.words(),
+      albumImage: coolImages.one(400, 400),
+      publishedYear: Math.floor(Math.random() * 69) + 1950,
+      songs: [],
+    };
+    const songNumber = Math.floor(Math.random() * 10) + 12;
+    for (let k = 0; k < songNumber; k += 1) {
+      const song = {
+        songID: i * 100 + j * 10 + k,
+        songName: faker.random.words(),
+        streams: Math.floor(Math.random() * 250000000),
+        length: Math.floor(Math.random() * 221) + 30,
+        popularity: Math.floor(Math.random() * 8) + 1,
+        addedToLibrary: faker.random.boolean(),
+      };
+      album.songs.push(song);
     }
-    var albumNumber = Math.floor(Math.random() * 4) + 1;
-    for (let j = 1; j < albumNumber + 1; j++) {
-      let album = {
-        albumID: i * 10 + j,
-        albumName: faker.random.words(),
-        albumImage: coolImages.one(400, 400),
-        publishedYear: Math.floor(Math.random() * 69) + 1950,
-        songs: []
-      }
-      var songNumber = Math.floor(Math.random() * 10) + 12;
-      for (let k = 1; k < songNumber + 1; k++) {
-        let song = {
-          songID: i * 100 + j * 10 + k,
-          songName: faker.random.words(),
-          streams: Math.floor(Math.random() * 250000000),
-          length: Math.floor(Math.random() * 221) + 30,
-          popularity: Math.floor(Math.random() * 8) + 1,
-          addedToLibrary: faker.random.boolean()
-        }
-        album.songs.push(song);
-      }
-      artist.albums.push(album);
-    }
-    allArtists.push(artist);
+    artist.albums.push(album);
   }
-
-  fs.writeFile('data.json', JSON.stringify(allArtists), 'utf8', (err) => {
-    if (err) throw err;
-    console.log("File written!");
-  })
-};
-
-module.exports.generateData = generateData;
-
+  stream.write(JSON.stringify(artist) + (i !== MAX ? ',' : ''));
+}
+stream.write(']');
+stream.end(() => {
+  console.log(counter, 'albums created');
+});
