ed3143f4a8b349061584ac9bf8b2765ca9d95a12
Shane Tajima <shanetajima@gmail.com>
2018-10-04 11:48:16 -0700

    Increase query timeout

--- seed_data/data/nosql/load_artists.sh
+++ seed_data/data/nosql/load_artists.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for file in ./artists_*.csv
 do
-  ./cassandra-loader -f "$file" -host 172.17.0.2 -delim "|" -charsPerColumn 8000 -schema "spottyfi.artists(album_id, artist_id, artist_name, album_name, album_image, published_year, songs)"  >> results.out
+  ./cassandra-loader -f "$file" -host 172.17.0.2 -delim "|" -queryTimeout 10 -charsPerColumn 8000 -schema "spottyfi.artists(album_id, artist_id, artist_name, album_name, album_image, published_year, songs)"  >> results.out
 done
