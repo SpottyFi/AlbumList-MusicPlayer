37cf4c5419554f59b3cfb573637b0406d5b601da
Shane Tajima <shanetajima@gmail.com>
2018-10-05 11:35:52 -0700

    Remove old files and folders

--- seed_data/data/nosql/load_artists.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for file in ./artists_*.csv
-do
-  ./cassandra-loader -f "$file" -host 172.17.0.2 -delim "|" -queryTimeout 10 -charsPerColumn 8000 -schema "spottyfi.artists(album_id, artist_id, artist_name, album_name, album_image, published_year, songs)"  >> results.out
-done
